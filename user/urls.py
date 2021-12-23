2c597656f098cbef215ffeb15daf7f448655effe
Venkatesh Potella <venky0895@gmail.com>
2021-12-23 22:09:45 +0530

    Modify - added get token api

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ app_name = "user"
 
 urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create-user"),
-    path("token/", views.LoginView.as_view(), name="token"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("token/", views.get_access_token, name="token"),
     path("users/", views.UserList.as_view(), name="user-list"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from django.utils.decorators import method_decorator
+
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework import exceptions
+import jwt
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 from user.auth import generate_access_token, generate_refresh_token
@@ -21,6 +29,7 @@ class LoginView(APIView):
     permission_classes = (AllowAny,)
     authentication_classes = []
 
+    @method_decorator(ensure_csrf_cookie)
     def post(self, request):
         """Authenticates a user with provided credentials"""
         serializer = AuthTokenSerializer(data=request.data)
@@ -41,6 +50,32 @@ class LoginView(APIView):
             return response
 
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@authentication_classes([])
+@permission_classes([AllowAny])
+@csrf_protect
+def get_access_token(request):
+    """returns refresh token for authenticated users"""
+    refresh_token = request.COOKIES.get('refreshtoken')
+    if refresh_token is None:
+        raise exceptions.AuthenticationFailed("Authentication credentials are not provided")
+
+    try:
+        jwt_payload = jwt.decode(refresh_token, settings.SECRET_KEY, algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+        raise exceptions.AuthenticationFailed("Refresh token is expired. Please login again")
+
+    user = get_user_model().objects.filter(id=jwt_payload["user_id"]).first()
+    if user is None:
+        raise exceptions.AuthenticationFailed("User not found")
+    elif not user.is_active:
+        raise exceptions.AuthenticationFailed("User is inactive")
+
+    user = UserSerializer(user).data
+    access_token = generate_access_token(user)
+    return Response({'access_token': access_token})
 
 
 class UserList(generics.ListAPIView):
